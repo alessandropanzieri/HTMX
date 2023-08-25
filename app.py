131a32bae9ff33829608e53c9c4496e84eeedbb5
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-08-25 18:39:09 +0200

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
@@ -8,5 +8,9 @@ def index():
         return "<h1>Hello</h1>"
 
     return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(_):
+    return redirect("/")
 
 if __name__ == "__main__": app.run(debug = True)
