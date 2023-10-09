166ef99d36bbecf53839e233ec616c97de8cb5cb
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-09 12:13:03 +0200

    commit

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, request, redirect, render_template
+from flask import (
+    Flask,
+    redirect,
+    render_template
+)
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET", "POST"])
-def index():
-    if request.method == "POST":
-        return "<h1>Hello</h1>"
-
+@app.get("/")
+def get():
     return render_template("index.html")
+
+@app.post("/")
+def post():
+    return "<h1>Hello</h1>"
 
 @app.errorhandler(404)
 def page_not_found(_):
     return redirect("/")
 
-if __name__ == "__main__": app.run(debug = True)
+if __name__ == "__main__": app.run()
